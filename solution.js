5705057e9edbbb2de64af21d7a1cd098375be528
Beardless-sheik <44676055+Beardless-sheik@users.noreply.github.com>
2019-06-19 14:19:52 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,1 +1,23 @@
 // solutions to the exercises mentioned in the readme should be in this file.
+function displayGreter(a,b){
+    if(a>b){
+        console.log(a+" is the larger value");
+    }
+    else
+    console.log(b+" is the larger value");
+}
+
+function loopIteration(){
+    "use strict";
+    for(let i=0;i<=15;i++){
+        if(i%2==0){
+            console.log(i+" is even");
+        }
+        else    
+            console.log(i+" is odd");
+    }
+}
+
+function sumMultiples(){
+    var multipleThree, multipleFive;
+}
